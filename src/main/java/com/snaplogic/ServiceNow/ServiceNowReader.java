abb9b682777e947aa72bf2e3f727a911cb06efc4
Daniel Dixon <ddixon@lunexa.com>
2012-03-07 14:13:52 -0500

    - Finished the suggest feature of the Read component. - Changed the Read component's query Property to only specify the   "sysparm_query" of the URL.
    --Daniel

--- src/main/java/com/snaplogic/ServiceNow/ServiceNowReader.java
+++ src/main/java/com/snaplogic/ServiceNow/ServiceNowReader.java
@@ -5,10 +5,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.snaplogic.cc.Capabilities;
 import org.snaplogic.cc.Capability;
@@ -17,7 +20,6 @@ import org.snaplogic.cc.InputView;
 import org.snaplogic.cc.OutputView;
 import org.snaplogic.cc.prop.SimpleProp;
 import org.snaplogic.cc.prop.SimpleProp.SimplePropType;
-import org.snaplogic.codehaus.jackson.JsonFactory;
 import org.snaplogic.codehaus.jackson.JsonParseException;
 import org.snaplogic.common.ComponentResourceErr;
 import org.snaplogic.common.Field;
@@ -62,87 +64,95 @@ public class ServiceNowReader extends ComponentAPI {
 	@Override
 	public void createResourceTemplate() {
 		// Initialize User Defined Properties
-		
-		setPropertyDef("connection", new SimpleProp("ServiceNow Connection Resource URI",
-				SimplePropType.SnapString, "Connection resource", true));
-		
-		setPropertyDef("query", new SimpleProp("ServiceNow Query",
-				SimplePropType.SnapString, "ServiceNow Query", true));
 
-		// Initialize Input View
-		//ArrayList<Field> infields = new ArrayList<Field>();
-		//infields.add(new Field("Domain", Field.SnapFieldType.SnapString, "Domain Name"));
-		//addRecordInputViewDef("Input", infields, "Default Input View", false);
-		
-		// Initialize Output View
-		//ArrayList<Field> fields = new ArrayList<Field>();
-		//fields.add(new Field("active", Field.SnapFieldType.SnapString,
-		//		"Active"));
-		//fields.add(new Field("number", Field.SnapFieldType.SnapNumber,
-			//	"ID"));
-		//addRecordOutputViewDef("Output", fields, "Default Output View", false);
+		setPropertyDef("connection", new SimpleProp(
+				"ServiceNow Connection Resource URI",
+				SimplePropType.SnapString, "Connection resource", true));
+
+		setPropertyDef(
+				"query",
+				new SimpleProp(
+						"ServiceNow Query",
+						SimplePropType.SnapString,
+						"ServiceNow Encoded Query Strings. See: http://wiki.service-now.com/index.php?title=Embedded:Encoded_Query_Strings",
+						true));
 	}
-	
+
+	/**
+	 * List of JSON fields that can be returned by the ServiceNow server.
+	 * http:// wiki.service-now.com/index.php?title=JSON_Web_Service#
+	 * JSON_object_format
+	 */
+	private final String[] serviceNowJsonFields = { "closed_by", "category",
+			"escalation", "state", "location", "reassignment_count",
+			"time_worked", "order", "due_date", "number", "upon_approval",
+			"sla_due", "follow_up", "notify", "business_stc", "caused_by",
+			"rejection_goto", "assignment_group", "incident_state",
+			"opened_at", "wf_activity", "calendar_duration", "group_list",
+			"caller_id", "comments", "priority", "sys_id", "sys_updated_by",
+			"variables", "delivery_task", "sys_updated_on", "parent", "active",
+			"opened_by", "expected_start", "sys_meta", "watch_list", "company",
+			"upon_reject", "work_notes", "sys_created_by", "cmdb_ci",
+			"approval_set", "user_input", "sys_created_on", "contact_type",
+			"rfc", "approval_history", "activity_due", "severity",
+			"subcategory", "work_end", "closed_at", "close_notes",
+			"variable_pool", "business_duration", "knowledge", "approval",
+			"sys_mod_count", "problem_id", "calendar_stc", "work_start",
+			"sys_domain", "sys_response_variables", "correlation_id",
+			"sys_class_name", "short_description", "impact", "description",
+			"correlation_display", "urgency", "assigned_to", "made_sla",
+			"delivery_plan" };
+
 	public void suggestResourceValues(ComponentResourceErr errObj) {
+
 		List<Field> outputViewFields = new ArrayList<Field>();
-		
-		//loop add fields
-		outputViewFields.add(new Field("active", Field.SnapFieldType.SnapString));
-		outputViewFields.add(new Field("activity_due", Field.SnapFieldType.SnapString));
-		outputViewFields.add(new Field("approval", Field.SnapFieldType.SnapString));
-		outputViewFields.add(new Field("assigned_to", Field.SnapFieldType.SnapString));
-		outputViewFields.add(new Field("number", Field.SnapFieldType.SnapString));
-		
+
+		// Add JSON fields
+		for (String attribute : serviceNowJsonFields) {
+			outputViewFields.add(new Field(attribute,
+					Field.SnapFieldType.SnapString));
+		}
+
 		addRecordOutputViewDef("Output", outputViewFields, "Some Output", true);
 	}
 
-	public void execute(Map<String, InputView> inputViews, Map<String, OutputView> outputViews) {
+	public void execute(Map<String, InputView> inputViews,
+			Map<String, OutputView> outputViews) {
 
+		// Get user defined properties
 		String connection = getStringPropertyValue("connection");
-		String queryString = getStringPropertyValue("query");
-		
+		String query = getStringPropertyValue("query");
+
 		if (connection == null) {
 			throw new SnapComponentException("ServiceNow login details not set");
 		}
-		
-		//Getting a handle to the connection component with the credentials and get those values.
+
+		// Getting a handle to the connection component and get the credential
+		// values.
 		ResDef resdef = this.getLocalResourceObject(connection);
 		String host = resdef.getPropertyValue("server").toString();
 		String username = resdef.getPropertyValue("username").toString();
 		String password = resdef.getPropertyValue("password").toString();
-		
-		
-		//String host = "https://snaplogic.service-now.com/incident.do?JSON&";
-		//String queryString = "/incident.do?JSON&sysparm_action=getRecords&sysparm_query=active=true^category=hardware";
-		String authorizationString = "Basic "
-				+ Base64.encodeBytes((username+":"+password).getBytes());
-		
-		URL url;
+
 		OutputView outputView = outputViews.values().iterator().next();
 		Record outRec;
-		ObjectMapper mapper = new ObjectMapper();
-		
+
 		try {
-			url = new URL(host + queryString);
-			HttpURLConnection urlConn = (HttpURLConnection) url
-					.openConnection();
-			urlConn.setRequestProperty("Authorization", authorizationString);
-			
-			//read Json data from ServiceNow into Map object
-			Map<String,Object> userData = mapper.readValue(urlConn.getInputStream(), Map.class);
-			
-			for( String recordKey : userData.keySet() ) { 	// should only have one top-level record attribute
-				List incidents = (List)userData.get(recordKey);
-				Iterator i = incidents.iterator();  //iterate over each incident
-				while(i.hasNext()) {
-					Map<String, String> incident = (Map<String, String>)i.next();
-					outRec = outputView.createRecord();
-					
-					for( String attributeKey : incident.keySet() ) {
-						outRec.set(attributeKey, incident.get(attributeKey));
-					}
-					outputView.writeRecord(outRec);
+			URL url = new URL(
+					host
+							+ "/incident.do?JSON&sysparm_action=getRecords&sysparm_query="
+							+ query);
+			List incidents = this.getIncidents(url, username, password);
+
+			Iterator i = incidents.iterator(); // iterate over each incident
+			while (i.hasNext()) {
+				Map<String, String> incident = (Map<String, String>) i.next();
+				outRec = outputView.createRecord();
+
+				for (String attributeKey : incident.keySet()) {
+					outRec.set(attributeKey, incident.get(attributeKey));
 				}
+				outputView.writeRecord(outRec);
 			}
 
 		} catch (MalformedURLException e) {
@@ -152,7 +162,46 @@ public class ServiceNowReader extends ComponentAPI {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		outputView.completed();
+	}
+
+	/**
+	 * Helper method
+	 * 
+	 * @param host
+	 * @param query
+	 * @param authorizationString
+	 * @return
+	 * @throws org.codehaus.jackson.JsonParseException
+	 * @throws JsonMappingException
+	 * @throws IOException
+	 */
+	private List getIncidents(URL url, String username, String password)
+			throws org.codehaus.jackson.JsonParseException,
+			JsonMappingException, IOException {
+
+		// Open connection to server
+		HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+		String authorizationString = "Basic "
+				+ Base64.encodeBytes((username + ":" + password).getBytes());
+		urlConn.setRequestProperty("Authorization", authorizationString);
+
+		// read Json data from ServiceNow into Map object
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> userData = mapper.readValue(
+				urlConn.getInputStream(), Map.class);
+
+		/**
+		 * The following loop is not technically necessary, there should only be
+		 * one top-level attribute called "records"
+		 */
+		List incidents = new ArrayList();
+		for (String recordKey : userData.keySet()) {
+			incidents.addAll((List) userData.get(recordKey));
+			return incidents;
+		}
+
+		return incidents;
 	}
 }
